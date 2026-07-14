a7f9bb0d706116ced318375102269878c8785e0a
mlopez <50690481+flashlopez06@users.noreply.github.com>
2026-07-14 17:25:05 -0400

    Scaffold Astro project, config, and copy static assets from source site

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://reflectionsproductions.com',
+  build: { format: 'file' },
+  integrations: [
+    sitemap({
+      filter: (page) =>
+        !page.endsWith('/404.html') && !page.includes('apex-5040-3d'),
+    }),
+  ],
+});
