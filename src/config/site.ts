9ce6c40b6eb84c1a7ab4268fd1fc5d94a64cfe29
mlopez <50690481+flashlopez06@users.noreply.github.com>
2026-07-14 17:32:04 -0400

    Add Layout.astro shell and site config data layer

---
+++ src/config/site.ts
@@ -0,0 +1,24 @@
+export const SITE = {
+  name: 'Reflections Productions',
+  shortName: 'Reflections',
+  tagline: 'Productions',
+  phone: '786-504-2369',
+  phoneHref: 'tel:+17865042369',
+  email: 'operations@reflectionsproductions.com',
+  address: '23750 SW 132 Avenue, Princeton, FL 33032',
+  foundingYear: 1975,
+  nav: [
+    { label: 'Stage Fleet', href: '/stages/index.html' },
+    { label: 'Production', href: '/production/index.html' },
+    { label: 'Rates', href: '/rates.html' },
+    { label: 'Our Work', href: '/work/index.html' },
+    { label: 'About', href: '/about.html' },
+  ],
+  cta: { label: 'Request a Quote', href: '/quote.html' },
+  socials: [
+    { label: 'Instagram', href: 'https://www.instagram.com/reflectionsproductions/' },
+    { label: 'Facebook', href: 'https://www.facebook.com/ReflectionsProductions/' },
+    { label: 'Youtube', href: 'https://www.youtube.com/@reflectionsproductions8660' },
+    { label: 'X', href: 'https://x.com/Reflectionsprod' },
+  ],
+};
